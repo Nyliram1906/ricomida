3cb4dfa22928d03bb9e59ace46c7cd85013199aa
Marilyn <marilyn.villalobos@gmail.com>
2024-04-19 11:01:51 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+/* // Agrega un evento de clic al primer botón
+document.getElementById("boton1").addEventListener("click", function() {
+  alert("Mensaje del primer botón");
+});
+
+// Agrega un evento de clic al segundo botón
+document.getElementById("boton2").addEventListener("click", function() {
+  alert("Mensaje del segundo botón");
+}); */
+
+
+
+let botonEnviar = document.getElementById ("boton-enviar")
+ botonEnviar.addEventListener("click", ()=>{
+  alert ("enviado al correo")
+     /* alert ("enviado al correo")  */
+})
+
+let botonFavoritos = document.getElementById ("boton-favoritos")
+ botonFavoritos.addEventListener ("click", ()=>{
+     alert ("agregado a favoritos") 
+})
+
+
+
+/* Agrega un evento de clic al primer botón
+let botonEnviar = document.getElementById("boton-enviar");
+botonEnviar.addEventListener("click", () => {
+  console.log("El correo fue enviado correctamente");
+});
+ */
+
+/* Agrega un evento de clic al segundo botón
+  document.getElementById("boton-2").addEventListener("click", function() {
+    alert("Añadido a Favoritos");
+  });
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
